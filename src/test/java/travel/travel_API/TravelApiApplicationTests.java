e3a0642f3dee484a875f4a5d4c8cc1264d764c3f
TomCorson <corsontom@yahoo.com>
2021-03-19 08:48:36 -0400

    initial commit

---
+++ src/test/java/travel/travel_API/TravelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package travel.travel_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
