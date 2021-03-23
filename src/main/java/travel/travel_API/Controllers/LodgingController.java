acc2c5b8803e973d952201e8b4579baf49be0ede
TomCorson <corsontom@yahoo.com>
2021-03-22 22:32:40 -0400

    get all lodging

--- src/main/java/travel/travel_API/Controllers/LodgingController.java
+++ src/main/java/travel/travel_API/Controllers/LodgingController.java
@@ -1,12 +1,21 @@
 package travel.travel_API.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import travel.travel_API.Models.Lodging;
 import travel.travel_API.Services.LodgingService;
+
+import java.util.List;
 
 @RestController
 public class LodgingController {
 
     @Autowired
     private LodgingService lodgingService;
+
+    @GetMapping
+    public List<Lodging> getAllLodging(){
+        return lodgingService.getAllLodging();
+    }
 }
--- src/main/java/travel/travel_API/Services/LodgingService.java
+++ src/main/java/travel/travel_API/Services/LodgingService.java
@@ -1,7 +1,18 @@
 package travel.travel_API.Services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import travel.travel_API.Models.Lodging;
+import travel.travel_API.Repos.LodgingRepo;
+
+import java.util.List;
 
 @Service
 public class LodgingService {
+    @Autowired
+    LodgingRepo lodgingRepo;
+
+    public List<Lodging> getAllLodging() {
+       return lodgingRepo.findAll();
+    }
 }
