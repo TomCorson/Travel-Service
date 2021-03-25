31802105dcfb1b0dc3bdf483bce269eec0bfd096
TomCorson <corsontom@yahoo.com>
2021-03-25 14:38:05 -0400

    import dependencies, starting JWTSecurityConfig

--- src/main/java/travel/travel_API/SecurityConfig/JWTSecurityConfig.java
+++ src/main/java/travel/travel_API/SecurityConfig/JWTSecurityConfig.java
@@ -1,4 +1,14 @@
 package travel.travel_API.SecurityConfig;
 
-public class JWTSecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+@Configuration
+public class JWTSecurityConfig extends WebSecurityConfigurerAdapter {
+
 }
