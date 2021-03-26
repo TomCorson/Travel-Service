79a86b3f2b026d56bce2dc75dbbc881c7aa25105
TomCorson <corsontom@yahoo.com>
2021-03-26 10:02:51 -0400

    JwtAuthToken

--- src/main/java/travel/travel_API/SecurityModel/JwtAuthenticationToken.java
+++ src/main/java/travel/travel_API/SecurityModel/JwtAuthenticationToken.java
@@ -1,19 +1,16 @@
 package travel.travel_API.SecurityModel;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
-
-import java.util.Collection;
 
 public class JwtAuthenticationToken extends UsernamePasswordAuthenticationToken {
-    public JwtAuthenticationToken(Object principal, Object credentials) {
-        super(principal, credentials);
-    }
-
-    public JwtAuthenticationToken(Object principal, Object credentials, Collection<? extends GrantedAuthority> authorities) {
-        super(principal, credentials, authorities);
+    private String token;
+    public JwtAuthenticationToken(String token) {
+        super(null,null);
+        this.token = token;
     }
 
     public String getToken() {
+        return token;
     }
+
 }
