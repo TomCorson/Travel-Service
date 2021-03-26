e2bc1f2526b0a6f05ef6ffb14e34efabfcda18a0
TomCorson <corsontom@yahoo.com>
2021-03-26 09:34:59 -0400

    jwtAuthTokenFilter

--- src/main/java/travel/travel_API/Security/JwtAuthenticationTokenFilter.java
+++ src/main/java/travel/travel_API/Security/JwtAuthenticationTokenFilter.java
@@ -1,11 +1,34 @@
 package travel.travel_API.Security;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import travel.travel_API.SecurityModel.JwtAuthenticationToken;
 
-public class JwtAuthenticationTokenFilter {
-    public void setAuthenticationManager(AuthenticationManager authenticationManager) {
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationTokenFilter extends AbstractAuthenticationProcessingFilter {
+    public JwtAuthenticationTokenFilter(){super("/lodging/**");}
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws AuthenticationException, IOException, ServletException {
+        String header = httpServletRequest.getHeader("Authorization");
+        if(header == null || !header.startsWith("Token ")){
+            throw new RuntimeException("JWT token is missing");
+        }
+        String authenticationToken = header.substring(6);
+        JwtAuthenticationToken token = new JwtAuthenticationToken(authenticationToken);
+        return getAuthenticationManager().authenticate(token);
     }
 
-    public void setAuthenticationSuccessHandler(JwtSuccessHandler jwtSuccessHandler) {
-    }
+   @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request,response,chain,authResult);
+        chain.doFilter(request,response);
+   }
 }
