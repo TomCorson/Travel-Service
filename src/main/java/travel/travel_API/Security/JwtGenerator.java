91a1f218042116593097da6b5fbd40fe30df009a
TomCorson <corsontom@yahoo.com>
2021-03-26 14:49:20 -0400

    jwtgenrator

---
+++ src/main/java/travel/travel_API/Security/JwtGenerator.java
@@ -0,0 +1,22 @@
+package travel.travel_API.Security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+import travel.travel_API.SecurityModel.JwtUser;
+
+@Component
+public class JwtGenerator {
+    public String generate(JwtUser jwtUser){
+        Claims claims = Jwts.claims()
+                .setSubject(jwtUser.getUserName());
+        claims.put("userId",String.valueOf(jwtUser.getUserId()));
+        claims.put("role",jwtUser.getRole());
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .signWith(SignatureAlgorithm.HS384,"ThisIsNotAVerySecureSecret")
+                .compact();
+    }
+}
